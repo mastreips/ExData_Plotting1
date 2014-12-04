9f077425b2746ff1c091afa1fb68e0476606b7ee
Marcus A. Streips <mastreips@gmail.com>
2014-12-03 22:44:53 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,13 +7,11 @@ data<-read.table("household_power_consumption.txt", sep=";",header=TRUE,
                  na.strings=c("NA","-","?"))
 data2<-filter(data, Date=="1/2/2007" | Date=="2/2/2007")
 data2$Date <-dmy(data2$Date)
-# data2$Date <-wday(data2$Date, label=TRUE, abbr=TRUE)
-# data2$Date <-wday(data2$Time, label=TRUE, abbr=TRUE)
-# plot(data2$Date, data2$Global_active_power)
+
 
 data2[,1]<-paste(data2[,1],data2[,2])
 data2[,1]<-as.POSIXct(data2$Date, format="%Y-%m-%d %H:%M:%S")
-# plot(data2$Date, data2$Global_active_power,type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
 par(ps=12)
 with(data2,{
   plot(data2$Date, data2$Sub_metering_1,type="l",  xlab="",
